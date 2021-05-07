f0a471551f3d1416f4aab7f456a43674c87067f3
Stephan Arulthasan <stephan.thasan@gmail.com>
2021-05-06 20:46:52 -0400

    add original pcluster+vpc templates, deploy script

---
+++ deploy-cryosparc.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+while [[ ""$#"" -gt 0 ]]; do
+    case $1 in
+        --region) AWS_REGION="$2"; shift ;;
+        --cluster-name) STACK_NAME="$2"; shift ;;
+        --az) COMPUTE_AZ="$2"; shift ;;
+        --config-bucket) CONFIG_BUCKET_NAME="$2"; shift ;;
+        --data-bucket) DATA_BUCKET_NAME="$2"; shift ;;
+        --aws-key) AWS_KEY="$2"; shift ;;
+        *) echo "Unknown parameter: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+VPC_STACK_NAME=${STACK_NAME}-VPC
+
+# Deploy CloudFormation template to set up VPC and subnets
+echo "Deploying CryoSPARC CloudFormation stack"
+
+aws cloudformation deploy --region ${AWS_REGION} \
+                          --template-file vpc-cryosparc.template \
+                          --stack-name ${VPC_STACK_NAME} \
+                          --parameter-override AvailabilityZone=${COMPUTE_AZ} CryoSPARCConfigBucket=${CONFIG_BUCKET_NAME}
+aws cloudformation wait stack-create-complete --stack-name ${VPC_STACK_NAME} --region ${AWS_REGION}
+
+# Get VPC and Subnet IDs
+VPC_ID=$(aws cloudformation describe-stacks --region ${AWS_REGION} --stack-name ${VPC_STACK_NAME} --query "Stacks[0].Outputs[?OutputKey=='VpcId'].OutputValue" --output text)
+echo "VPC ID: ${VPC_ID}"
+COMPUTE_SUBNET_ID=$(aws cloudformation describe-stacks --region ${AWS_REGION} --stack-name ${VPC_STACK_NAME} --query "Stacks[0].Outputs[?OutputKey=='PrivateSubnetId'].OutputValue" --output text)
+echo "Compute Subnet ID: ${COMPUTE_SUBNET_ID}"
+MASTER_SUBNET_ID=$(aws cloudformation describe-stacks --region ${AWS_REGION} --stack-name ${VPC_STACK_NAME} --query "Stacks[0].Outputs[?OutputKey=='PublicSubnetId'].OutputValue" --output text)
+echo "Master Subnet ID: ${MASTER_SUBNET_ID}"
+
+# Edit add VPC and Subnet IDs to ParallelCluster configuration
+cp cryosparc-pcluster.config.template cryosparc-pcluster.config
+gsed -i "s|@AWS_REGION@|${AWS_REGION}|g" cryosparc-pcluster.config
+gsed -i "s|@AWS_KEY@|${AWS_KEY}|g" cryosparc-pcluster.config
+gsed -i "s|@VPC_ID@|${VPC_ID}|g" cryosparc-pcluster.config
+gsed -i "s|@COMPUTE_SUBNET_ID@|${COMPUTE_SUBNET_ID}|g" cryosparc-pcluster.config
+gsed -i "s|@MASTER_SUBNET_ID@|${MASTER_SUBNET_ID}|g" cryosparc-pcluster.config
+gsed -i "s|@S3_CONFIG_BUCKET@|${CONFIG_BUCKET_NAME}|g" cryosparc-pcluster.config
+gsed -i "s|@S3_DATA_BUCKET@|${DATA_BUCKET_NAME}|g" cryosparc-pcluster.config
+
+# Create ParallelCluster
+pcluster create --region ${AWS_REGION} -nr -c cryosparc-pcluster.config ${STACK_NAME}
+
+CRYOSPARC_HEADNODE_IPADDR=$(aws ec2 describe-instances --region $AWS_REGION --filters Name=tag:ClusterName,Values=cryosparc Name=tag:Name,Values=Master --query 'Reservations[].Instances[].NetworkInterfaces[].Association[].PublicIp' --output text)
+echo ""
+echo ""
+echo "CryoSPARC cluster configuration complete. You can login using the following command:"
+echo "(Be sure to use the correct path the SSH key you provided)"
+echo ""
+echo "ssh -i /path/to/key/${AWS_KEY} ec2-user@${CRYOSPARC_HEADNODE_IPADDR}"
+echo ""
+echo ""
+echo "Once logged in, you will need to create a new user with the command:"
+echo ""
+echo "cryosparcm createuser --email "<email_address>" --password "<password>" --username "<username>"   --firstname "<user first name>" --lastname "<user lasname>""
+echo ""
+echo ""
+echo "Once your user is created, you need to set up an SSH tunnel to the CryoSPARC head node"
+echo "that will be used to connect to the CryoSPARC web interface:"
+echo ""
+echo "ssh -i /path/to/key/${AWS_KEY} -N -f -L localhost:45000:localhost:45000 ec2-user@${CRYOSPARC_HEADNODE_IPADDR}"
+echo ""
+echo "You can then open a web browser to http://localhost:45000"
+echo "and log into CryoSPARC."
+echo ""
---
+++ install-cryosparc.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+. "/etc/parallelcluster/cfnconfig"
+
+if [ "${cfn_node_type}" == "MasterServer" ]; then
+   CUDA_INSTALL_PATH=/shared/cuda
+   CRYOSPARC_INSTALL_PATH=/shared/cryosparc
+   LICENSE_ID=""
+   yum -y update
+
+   # Install CUDA Toolkit (10.2 for CryoSPARC)
+   mkdir -p ${CUDA_INSTALL_PATH}
+   cd ${CUDA_INSTALL_PATH}
+   wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
+   sh cuda_10.2.89_440.33.01_linux.run --defaultroot=/shared/cuda --toolkit --toolkitpath=${CUDA_INSTALL_PATH}/cuda-10.2 --samples --silent
+   rm cuda_10.2.89_440.33.01_linux.run
+
+   # Add CUDA to the path
+   cat > /etc/profile.d/cuda.sh << 'EOF'
+PATH=$PATH:@CUDA_INSTALL_PATH@/cuda-10.2/bin
+EOF
+   sed -i "s|@CUDA_INSTALL_PATH@|${CUDA_INSTALL_PATH}|g" /etc/profile.d/cuda.sh
+   . /etc/profile.d/cuda.sh
+
+   # Download cryoSPARC
+   mkdir -p ${CRYOSPARC_INSTALL_PATH}
+   cd ${CRYOSPARC_INSTALL_PATH}
+   curl -L https://get.cryosparc.com/download/master-latest/${LICENSE_ID} -o cryosparc_master.tar.gz
+   curl -L https://get.cryosparc.com/download/worker-latest/${LICENSE_ID} -o cryosparc_worker.tar.gz
+
+   # Install master process
+   tar -xf cryosparc_master.tar.gz
+   cd cryosparc_master
+   ./install.sh --license ${LICENSE_ID} \
+      --hostname ${HOSTNAME} \
+      --dbpath ${CRYOSPARC_INSTALL_PATH}/cryosparc_db \
+      --port 45000 \
+      --allowroot \
+      --yes
+
+   # Start cryoSPARC master package
+   ${CRYOSPARC_INSTALL_PATH}/cryosparc_master/bin/cryosparcm start
+
+   # Add CryoSPARC to the path
+   cat > /etc/profile.d/cryosparc.sh << 'EOF'
+PATH=$PATH:@CRYOSPARC_INSTALL_PATH@/cryosparc_master/bin
+EOF
+   sed -i "s|@CRYOSPARC_INSTALL_PATH@|${CRYOSPARC_INSTALL_PATH}|g" /etc/profile.d/cryosparc.sh
+   . /etc/profile.d/cryosparc.sh
+
+
+   echo "export CRYOSPARC_FORCE_USER=true" >> ${CRYOSPARC_INSTALL_PATH}/cryosparc_master/config.sh
+   echo "export CRYOSPARC_FORCE_HOSTNAME=true" >> ${CRYOSPARC_INSTALL_PATH}/cryosparc_master/config.sh
+
+   # Install cryoSPARC work package
+   cd ${CRYOSPARC_INSTALL_PATH}
+   tar -xf cryosparc_worker.tar.gz
+   cd cryosparc_worker
+   ./install.sh --license ${LICENSE_ID} \
+      --cudapath ${CUDA_INSTALL_PATH}/cuda-10.2 \
+      --yes
+
+   # Create cluster config files
+   cat > ${CRYOSPARC_INSTALL_PATH}/cluster_info.json << 'EOF'
+{
+"qdel_cmd_tpl": "scancel {{ cluster_job_id }}",
+"worker_bin_path": "@CRYOSPARC_INSTALL_PATH@/cryosparc_worker/bin/cryosparcw",
+"title": "cryosparc-cluster",
+"cache_path": "",
+"qinfo_cmd_tpl": "sinfo",
+"qsub_cmd_tpl": "sbatch {{ script_path_abs }}",
+"qstat_cmd_tpl": "squeue -j {{ cluster_job_id }}",
+"send_cmd_tpl": "{{ command }}",
+"name": "gpu"
+}
+EOF
+
+   sed -i "s|@CRYOSPARC_INSTALL_PATH@|${CRYOSPARC_INSTALL_PATH}|g" ${CRYOSPARC_INSTALL_PATH}/cluster_info.json
+
+   cat > ${CRYOSPARC_INSTALL_PATH}/cluster_script.sh << 'EOF'
+#!/bin/bash
+#SBATCH --job-name=cryosparc_{{ project_uid }}_{{ job_uid }}
+#SBATCH --output={{ job_log_path_abs }}
+#SBATCH --error={{ job_log_path_abs }}
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task={{ num_cpu }}
+#SBATCH --gres=gpu:{{ num_gpu }}
+#SBATCH --partition=gpu
+{{ run_cmd }}
+EOF
+
+   rm ${CRYOSPARC_INSTALL_PATH}/*.tar.gz
+   chown -R ec2-user: /shared/cryosparc
+
+   # Start cluster
+   /bin/su -c "${CRYOSPARC_INSTALL_PATH}/cryosparc_master/bin/cryosparcm start" - ec2-user
+
+   # Connect worker nodes to cluster
+   /bin/su -c "cd ${CRYOSPARC_INSTALL_PATH} && ${CRYOSPARC_INSTALL_PATH}/cryosparc_master/bin/cryosparcm cluster connect" - ec2-user
+
+   # Restart master
+   /bin/su -c "cd ${CRYOSPARC_INSTALL_PATH} && ${CRYOSPARC_INSTALL_PATH}/cryosparc_master/bin/cryosparcm restart" - ec2-user
+
+fi
